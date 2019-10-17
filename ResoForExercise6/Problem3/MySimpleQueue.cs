9f15eb69f147d8fd387b78049b0692e80710611b
minhtamtkst21 <minhtamtruongkhanh.st@gmail.com>
2019-10-17 14:58:54 +0700

    Comple Problem3

---
+++ ResoForExercise6/Problem3/MySimpleQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class MySimpleQueue
+    {
+        private int[] arr;
+
+        public MySimpleQueue()
+        {
+            arr = new int[0];
+        }
+
+        public int Count
+        {
+            get { return arr.Length; }
+        }
+
+        public void Enqueue(int item)
+        {
+            ReAllocMemory("Enqueue");
+            arr[Count - 1] = item;
+        }
+
+        public int Dequeue()
+        {
+            int value = arr[0];
+            ReAllocMemory("Dequeue");
+            return value;
+        }
+
+        public int Peek()
+        {
+            return arr[0];
+        }
+
+        private void ReAllocMemory(string act)
+        {
+            if (act == "Enqueue")
+            {
+                Array.Resize(ref arr, Count + 1);
+            }else if (act == "Dequeue")
+            {
+                int[] temp = new int[Count - 1];
+                Array.Copy(arr, 1, temp, 0, temp.Length);
+                arr = temp;
+            }
+        }
+    }
+}
---
+++ ResoForExercise6/Problem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySimpleQueue Q = new MySimpleQueue();
+            Q.Enqueue(1);	//Queue contains 1
+            Q.Enqueue(2);	//Queue contains 1 2
+            Q.Enqueue(3);	//Queue contains 1 2 3
+            Console.WriteLine(Q.Dequeue()); //Prints 1, queue contains 1 2
+            Console.WriteLine(Q.Dequeue()); //Prints 2, queue contains 1
+            Console.WriteLine(Q.Count);
+        }
+    }
+}
