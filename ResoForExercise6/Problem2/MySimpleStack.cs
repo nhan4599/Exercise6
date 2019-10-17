42b9b55b17495546860d4c0c48f43999b10fa355
nguyen tri nhan ho <nhan01628483955@gmail.com>
2019-10-17 09:43:51 +0700

    Complete Problem2

---
+++ ResoForExercise6/Problem2/MySimpleStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problem2
+{
+    class MySimpleStack
+    {
+        private int[] arr;
+
+        public int Count => arr.Length;
+
+        public MySimpleStack()
+        {
+            arr = new int[0];
+        }
+
+        public void Put(int item)
+        {
+            ReAllocMemory("PUT");
+            arr[Count - 1] = item;
+        }
+
+        public int Pop()
+        {
+            int value = arr[Count - 1];
+            ReAllocMemory("POP");
+            return value;
+        }
+
+        public int Peek()
+        {
+            return arr[Count - 1];
+        }
+
+        private void ReAllocMemory(string act)
+        {
+            if (act == "PUT")
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+            }else if (act == "POP")
+            {
+                Array.Resize(ref arr, arr.Length - 1);
+            }
+        }
+    }
+}
---
+++ ResoForExercise6/Problem2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySimpleStack S = new MySimpleStack();
+            S.Put(1);   //Stack contains 1
+            S.Put(2);   //Stack contains 1 2
+            S.Put(3);   //Stack contains 1 2 3
+            Console.WriteLine(S.Pop()); //Prints 3, stack contains 1 2
+            Console.WriteLine(S.Pop()); //Prints 2, stack contains 1
+            Console.WriteLine(S.Count);
+        }
+    }
+}
